a0e4a2daf5449cbc2fb227ed9a482f850b0d7c0b
LucasB25 <battistolo..lucas@gmail.com>
2022-06-01 11:34:46 +0200

    Update send-panel.js

--- slashCommands/admin/send-panel.js
+++ slashCommands/admin/send-panel.js
@@ -2,12 +2,12 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
   name: "send-panel",
-  description: "Send ticket panel to specific channel!",
+  description: "Send ticket panel to specific channel.",
   usage: "/send-panel <channel>",
   dir: "message",
   cooldown: 1,
-  examples: ["send-panel", "send-panel channel:welcome"],
-  liases: ["send-ticket"],
+  examples: ["send-panel", "send-panel channel:ticket"],
+  aliases: ["send-ticket"],
   permissions: ["ADMINISTRATOR"],
   ownerOnly: true,
   options: [
@@ -22,30 +22,33 @@ module.exports = {
   run: async (client, interaction) => {
     const channel = interaction.options.getChannel("channel");
 
-    const row = new MessageActionRow().addComponents(
-      new MessageButton()
-        .setStyle("SECONDARY")
-        .setEmoji("📩")
-        .setCustomId("create-ticket")
-    );
-
-    const embed = new MessageEmbed()
-      .setTitle("Create ticket")
-      .setDescription(
-        "Do you need any help or request from the founder or an administrator?\n\n" +
-          "To create a ticket react with 📩"
-      )
-      .setAuthor({
-        name: client.user.username,
-        iconURL: client.user.displayAvatarURL(),
-      })
-      .setThumbnail(client.user.displayAvatarURL())
-      .setColor(client.config.embedColor);
-
+    interaction.channel.send({
+      components: [
+        new MessageActionRow().addComponents(
+          new MessageButton()
+            .setStyle("SECONDARY")
+            .setEmoji("📩")
+            .setCustomId("create-ticket")
+        ),
+      ],
+      embeds: [
+        new MessageEmbed()
+          .setTitle("Create ticket")
+          .setDescription(
+            "Do you need any help or request from the founder or an administrator?\n\n" +
+              "To create a ticket react with 📩"
+          )
+          .setAuthor({
+            name: client.user.username,
+            iconURL: client.user.displayAvatarURL(),
+          })
+          .setThumbnail(client.user.displayAvatarURL())
+          .setColor(client.config.embedColor),
+      ],
+    });
     interaction.reply({
       content: `Ticket panel success send to ${channel}!`,
       ephemeral: true,
     });
-    return channel.send({ embeds: [embed], components: [row] });
   },
 };
